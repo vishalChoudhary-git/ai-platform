a37ac48f50436cb44cbe7296a7d0bdd8b19eafe4
Vishal Choudhary <80300403+vishalChoudhary-git@users.noreply.github.com>
2026-08-18 08:50:24 +0530

    feat: export expense plugin schemas

---
+++ app/plugins/expenses/schemas/__init__.py
@@ -0,0 +1,9 @@
+from .expense import ExpenseCreateData
+from .response import ExpenseApprovalResponse, ExpenseDocumentResponse, ExpenseResponse
+
+__all__ = [
+    "ExpenseCreateData",
+    "ExpenseApprovalResponse",
+    "ExpenseDocumentResponse",
+    "ExpenseResponse",
+]
