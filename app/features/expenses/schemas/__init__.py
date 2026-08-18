ce327888652422ad9aa62134e4dcc45dcb683488
Vishal Choudhary <80300403+vishalChoudhary-git@users.noreply.github.com>
2026-08-18 08:53:56 +0530

    refactor: remove obsolete expense feature package

--- app/features/expenses/schemas/__init__.py
+++
@@ -1,8 +0,0 @@
-from .request import ExpenseCreateRequest
-from .response import ExpenseDocumentResponse, ExpenseResponse
-
-__all__ = [
-    "ExpenseCreateRequest",
-    "ExpenseDocumentResponse",
-    "ExpenseResponse",
-]
