0fccf7126e0792003daf8e84e559128898686da7
Vishal Choudhary <80300403+vishalChoudhary-git@users.noreply.github.com>
2026-08-25 07:38:54 +0530

    refactor: export environment email sender

--- app/core/notifications/__init__.py
+++ app/core/notifications/__init__.py
@@ -1,3 +1,15 @@
-from .email import EmailMessageData, EmailSender, SmtpEmailSender
+from .email import (
+    EmailMessageData,
+    EmailSender,
+    MailtrapEmailSender,
+    SmtpEmailSender,
+    get_email_sender,
+)
 
-__all__ = ["EmailMessageData", "EmailSender", "SmtpEmailSender"]
+__all__ = [
+    "EmailMessageData",
+    "EmailSender",
+    "MailtrapEmailSender",
+    "SmtpEmailSender",
+    "get_email_sender",
+]
